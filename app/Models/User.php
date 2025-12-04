bcfc70cae4597557f5549e0520f327b8a3bf9cd1
Mohya972 <nfloh.noel@gmail.com>
2025-12-04 14:48:04 +0100

    push

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,10 +2,11 @@
 
 namespace App\Models;
 
+use App\Models\Cart;
 use App\Enums\UserRole;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Database\Eloquent\Relations\HasOne;
 
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
