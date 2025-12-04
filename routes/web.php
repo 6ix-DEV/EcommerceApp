782645587297669ecee777437e6f146288603270
Mohya972 <nfloh.noel@gmail.com>
2025-12-04 16:16:07 +0100

    commit_16h

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+/*
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,5 +16,33 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+require __DIR__.'/auth.php';*/
+
+
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CartController;
+
+// Page d'accueil
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+// Catalogue produits
+Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+Route::get('/products/{product:slug}', [ProductController::class, 'show'])->name('products.show');
+
+// Catégories
+Route::get('/categories/{category:slug}', [ProductController::class, 'category'])->name('categories.show');
+
+/*// Panier (authentification requise)
+Route::middleware(['auth'])->group(function () {
+    Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
+    Route::post('/cart/add/{product}', [CartController::class, 'add'])->name('cart.add');
+    Route::patch('/cart/update/{cartItem}', [CartController::class, 'update'])->name('cart.update');
+    Route::delete('/cart/remove/{cartItem}', [CartController::class, 'remove'])->name('cart.remove');
+    Route::delete('/cart/clear', [CartController::class, 'clear'])->name('cart.clear');
+});*/
 
 require __DIR__.'/auth.php';
